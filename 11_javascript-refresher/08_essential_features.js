8d1f8bcb9966b70b0fc66a6c4cd915f2fe02db96
Cie <jocelynbendoy26@gmail.com>
2025-09-06 10:33:12 +0800

    Add essential features activity

---
+++ 11_javascript-refresher/08_essential_features.js
@@ -0,0 +1,19 @@
+let hobbies = ["Reading", "Traveling", "Gaming"];
+
+hobbies.map((hobbies) =>{ console.log(hobbies); });
+
+const person = {
+    name: "Jocelyn",
+    age: 25,
+}
+
+let {name, age} = person
+
+console.log(person);
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5];
+
+const combined = [...arr1, ...arr2];
+console.log(combined);
+
