cfcb5aa438ad60d22bf80c4b737bdd4ee8966ba1
Cie <jocelynbendoy26@gmail.com>
2025-09-16 23:50:39 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -7,8 +7,8 @@ import './App.css'
 
 // User data
 const user = {
-  name: 'Celine',
-  imageUrl: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*ucL7YQ2v8aaOy426soLPZA.png',
+  name: 'Jocelyn',
+  imageUrl: 'https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fsr0hy4omyh9hbyc9r1mh.png',
   imageSize: 150,
 };
 
@@ -44,7 +44,8 @@ export default function App() {
             alt={'Photo of ' + user.name}
             style={{
               width: user.imageWidth,
-              height: user.imageSize
+              height: user.imageSize,
+              borderRadius: '20px'
             }}
           />
         </>
