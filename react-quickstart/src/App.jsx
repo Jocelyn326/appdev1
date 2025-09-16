41f3d6f2954b1a031f6aac6d0ae9b262f88e6c7f
Cie <jocelynbendoy26@gmail.com>
2025-09-16 23:30:43 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -4,6 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
+
+// User data
+const user = {
+  name: 'Celine',
+  imageUrl: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*ucL7YQ2v8aaOy426soLPZA.png',
+  imageSize: 150,
+};
+
 // Button component
 function MyButton() {
   return <button>I'm a Button</button>;
@@ -26,9 +34,20 @@ export default function App() {
       <header>
         <h1>Welcome to my app</h1>
       </header>
-
       <MyButton />
       <AboutPage />
+        <>
+          <h1>{user.name}</h1>
+          <img
+            className="avatar"
+            src={user.imageUrl}
+            alt={'Photo of ' + user.name}
+            style={{
+              width: user.imageWidth,
+              height: user.imageSize
+            }}
+          />
+        </>
     </div>
   );
 }
