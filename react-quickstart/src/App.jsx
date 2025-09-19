ef91ae44203c7328cc663133512ae887b5105cb1
Cie <jocelynbendoy26@gmail.com>
2025-09-19 21:25:26 +0800

    Updating the screen / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -10,13 +10,15 @@ const user = {
 };
 
 function MyButton() {
+  const [count, setCount] = useState(0);
+
   function handleClick() {
-    alert('You clicked me!');
+    setCount(count + 1);
   }
 
   return (
     <button onClick={handleClick}>
-      Click me
+      Clicked {count} times
     </button>
   );
 }
