00bf132a02e1636855cfaec14ca5974457e87829
Cie <jocelynbendoy26@gmail.com>
2025-11-23 13:15:08 +0800

    Done with fetching todos data exercise using axios

--- react-useeffect-simple-api-fetch/src/App.jsx
+++ react-useeffect-simple-api-fetch/src/App.jsx
@@ -1,10 +1,12 @@
 import PostsFetchAsync from "./PostsFetchAsync";
+import TodosFetchAxios from "./TodosFetchAxios";
 
 export default function App() {
   return (
     <div>
       <PostsFetchAsync />
-   
+      <TodosFetchAxios />
+
     </div>
   );
 }
--- react-useeffect-simple-api-fetch/src/UsersFetch.jsx
+++ react-useeffect-simple-api-fetch/src/UsersFetch.jsx
@@ -1,40 +1,12 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import UsersFetch from "./UsersFetch";
 
-function UserFetch() {
-  const [userData, setUserData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users?_limit=5")
-      .then((response) => response.json())
-      .then((data) => {
-        setUserData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError("Error fetching user data:", error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>
-
+function App() {
   return (
     <div>
-      <h2>User Data</h2>
-      <ul>
-        {userData.map((user) => (
-          <li key={user.id}>
-            <p>Name: {user.name}</p>
-            <p>Email: {user.email}</p>
-          </li>
-        ))}
-      </ul>
+      <h1>React useEffect Exercises</h1>
+      <UsersFetch />
     </div>
   );
 }
 
-export default UserFetch;
+export default App;
